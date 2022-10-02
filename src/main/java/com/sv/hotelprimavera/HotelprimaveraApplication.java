f8e5bb4e307ed498bc9e837ce2cc6623f6a64806
Fernando Carranza <foreverfas_fc@hotmail.com>
2022-10-01 21:10:43 -0600

    first commit

---
+++ src/main/java/com/sv/hotelprimavera/HotelprimaveraApplication.java
@@ -0,0 +1,13 @@
+package com.sv.hotelprimavera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelprimaveraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelprimaveraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sv/hotelprimavera/HotelprimaveraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.hotelprimavera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelprimaveraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
